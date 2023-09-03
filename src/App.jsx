9d26464f63c46f84580994f27bfe5ff33286bf17
Eslam Wael <84073355+SolomDev00@users.noreply.github.com>
2023-09-03 22:05:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import Home from "./Pages/Website/Home";
-import Err404 from "./Pages/Errors/404";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Route, Routes } from "react-router-dom";
-import Loading from "./Components/Loading/Loading";
-import About from "./Pages/Website/About";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        {/* Tester Routes */}
-        <Route path="test" element={<Loading />} />
-        {/* Public Routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="/*" element={<Err404 />} />
-        {/* Protected Routes */}
-      </Routes>
+          <Home />
     </div>
   );
 }
